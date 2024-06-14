4fc889842221ccb7b256b7ca4457a078f65d490f
asif-mohmd <asifasifpsps@gmail.com>
2024-06-14 10:22:56 +0530

    Video player changed

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ WORKDIR /app
 
 COPY package.json .
 
-RUN npm install
+# Use --legacy-peer-deps to bypass peer dependency conflicts
+RUN npm install --legacy-peer-deps
 
 COPY . .
 
@@ -12,7 +13,7 @@ RUN npm run build
 
 EXPOSE 5000
 
-ENV VITE_APIURL = "https://geniusgrid.online/api"
-ENV stripeKey = "pk_test_51L06jrSBZHWEgXwypDrrO0AxMUyUaOCrMsapB1sLnEtIo3rGwMbWz7KP3phCvQz0PyaKwsvJR5puR2a17FANOfn900XouYmHR2"
+ENV VITE_APIURL="https://geniusgrid.online/api"
+ENV stripeKey="pk_test_51L06jrSBZHWEgXwypDrrO0AxMUyUaOCrMsapB1sLnEtIo3rGwMbWz7KP3phCvQz0PyaKwsvJR5puR2a17FANOfn900XouYmHR2"
 
-CMD [ "npm", "run", "preview" ]
+CMD ["npm", "run", "preview"]
--- src/components/User/Course/VideoPlayer.tsx
+++ src/components/User/Course/VideoPlayer.tsx
@@ -1,131 +1,109 @@
-// import { APITypes, PlyrProps, usePlyr } from "plyr-react";
-// import "plyr-react/plyr.css";
-// import Hls from "hls.js";
-// import { Options } from "plyr";
-// import React from "react";
-// const videoOptions = null;
-// const videoSource = null;
+import React, { useEffect, useRef, useState } from 'react';
+import Hls from 'hls.js';
+import '@vime/core/themes/default.css';
 
-
-// const useHls = (src: string, options: Options | null) => {
-//   const hls = React.useRef<Hls>(new Hls());
-//   const hasQuality = React.useRef<boolean>(false);
-//   const [plyrOptions, setPlyrOptions] = React.useState<Options | null>(options);
-
-//   React.useEffect(() => {
-//     hasQuality.current = false;
-//   }, [options]);
-
-//   React.useEffect(() => {
-//     hls.current.loadSource(src);
-//     hls.current.attachMedia(document.querySelector(".plyr-react")!);
-//     hls.current.on(Hls.Events.MANIFEST_PARSED, () => {
-//       if (hasQuality.current) return;
-//       const levels = hls.current.levels;
-//       const quality: Options["quality"] = {
-//         default: levels[levels.length - 1].height,
-//         options: levels.map((level) => level.height),
-//         forced: true,
-//         onChange: (newQuality: number) => {
-//           levels.forEach((level, levelIndex) => {
-//             if (level.height === newQuality) {
-//               hls.current.currentLevel = levelIndex;
-//             }
-//           });
-//         },
-//       };
-
-//       setPlyrOptions({ ...plyrOptions, quality });
-//       hasQuality.current = true;
-//     });
-//   });
-
-//   return { options: plyrOptions };
-// };
-
-
-// const CustomPlyrInstance = React.forwardRef<
-//   APITypes,
-//   PlyrProps & { hlsSource: string, subtitleUrl: string }
-// >((props, ref) => {
-//   const { source, options = null, hlsSource, subtitleUrl } = props;
-//   const raptorRef = usePlyr(ref, {
-//     ...useHls(hlsSource, options),
-//     source,
-//   }) as React.MutableRefObject<HTMLVideoElement>;
-//   return (
-//     <video  crossOrigin="anonymous" ref={raptorRef} className="plyr-react plyr">
-//       <track
-//         kind="captions"
-//         label="English"
-//         src={subtitleUrl}
-//         srcLang="en"
-//         default={true}
-//       />
-//     </video>
-//   );
-// });
-// type Props = {
-//   videoUrl : string;
-//   subtitleUrl: string;
-// }
-// const VideoPlayer:React.FC<Props> = ({videoUrl, subtitleUrl}) => {
-//   const ref = React.useRef<APITypes>(null);
-//   const supported = Hls.isSupported();
-
-//   return (
-//     <>
-//     <div className="wrapper">
-//       {supported ? (
-//         <CustomPlyrInstance
-//           ref={ref}
-//           source={videoSource}
-//           options={videoOptions}
-//           hlsSource={videoUrl}
-//           subtitleUrl={subtitleUrl}
-//         />
-//       ) : (
-//         "HLS is not supported in your browser"
-//       )}
-//     </div>
-
-    
-
-//     </>
-//   );
-// };
-
-// CustomPlyrInstance.displayName = 'CustomPlyrInstance';
-
-// export default VideoPlayer;
-
-
-import { Player, Hls, DefaultUi, DefaultControls} from '@vime/react';
 type Props = {
   videoUrl: string;
-  subtitleUrl: string;
-}
+  subtitleUrl?: string;
+};
 
-const VideoPlayer: React.FC<Props> = ({ videoUrl }) => {
+const VideoPlayer: React.FC<Props> = ({ videoUrl, subtitleUrl }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [qualityLevels, setQualityLevels] = useState<any[]>([]);
+  const [currentQuality, setCurrentQuality] = useState<number>(-1); // -1 means auto
+  const [isHovered, setIsHovered] = useState<boolean>(false);
 
-  const hlsConfig = {
-    // ...
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        // This will work in Safari, where HLS is natively supported
+        video.src = videoUrl;
+      } else if (Hls.isSupported()) {
+        // This will work in other browsers with hls.js
+        const hls = new Hls();
+        hls.loadSource(videoUrl);
+        hls.attachMedia(video);
+
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+          setQualityLevels(hls.levels);
+        });
+
+        hls.on(Hls.Events.LEVEL_SWITCHED, (_event, data) => {
+          setCurrentQuality(data.level);
+        });
+
+        video.addEventListener('qualitychange', (event: any) => {
+          const selectedQuality = event.detail.quality;
+          if (selectedQuality === -1) {
+            hls.currentLevel = -1; // auto
+          } else {
+            hls.currentLevel = selectedQuality;
+          }
+        });
+      } else {
+        console.error('HLS is not supported in this browser.');
+      }
+    }
+  }, [videoUrl]);
+
+  const handleQualityChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedQuality = parseInt(event.target.value, 10);
+    const qualityChangeEvent = new CustomEvent('qualitychange', {
+      detail: { quality: selectedQuality },
+    });
+    videoRef.current?.dispatchEvent(qualityChangeEvent);
   };
 
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
   return (
-    <Player >
- <Hls version="latest" config={hlsConfig} poster="/media/poster.png">
-        <source data-src={videoUrl} type="application/x-mpegURL" />
-      </Hls>
-
-      <DefaultUi noControls>
-        {/* We setup the default controls and pass in any options. */}
-        <DefaultControls hideOnMouseLeave activeDuration={2000} />
-      </DefaultUi>
+    <div 
+      className="relative w-full pt-[56.25%]" 
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <video
+        ref={videoRef}
+        controls
+        className="absolute top-0 left-0 w-full h-full"
+        poster="/media/poster.png"
+      >
+        {subtitleUrl && (
+          <track
+            default
+            kind="subtitles"
+            src={subtitleUrl}
+            srcLang="en"
+            label="English"
+          />
+        )}
+      </video>
       
-    </Player>
+      {((isHovered && window.innerWidth > 768) || window.innerWidth <= 768) && qualityLevels.length > 0 && (
+        <div className="absolute top-1 right-2 z-10 ">
+          <select
+            onChange={handleQualityChange}
+            value={currentQuality}
+            className="bg-gray-800 text-white p-0.5 font-semibold  rounded-lg border border-gray-600 appearance-none opacity-50"
+          >
+            <option value="-1">Auto</option>
+            {qualityLevels.map((level, index) => (
+              <option key={index} value={index}>
+                {level.height}p
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+    </div>
   );
-}
+};
 
 export default VideoPlayer;
