1e2bb911f17a7c8483830d924bfe7329e32c884b
asif-mohmd <asifasifpsps@gmail.com>
2024-06-14 16:24:10 +0530

    CI CD test 2

--- src/components/User/Home/HomePage.tsx
+++ src/components/User/Home/HomePage.tsx
@@ -13,7 +13,7 @@ const HomePage: React.FC = () => {
         <div className="md:w-1/2 p-6 md:p-12 md:order-first"> {/* Order first on medium screens and up */}
           <h1 className="my-3 text-2xl md:text-3xl lg:text-5xl font-bold font-poppins">Learning comes to youuu</h1>
           <p className="text-sm md:text-lg lg:text-md font-poppins md:mt-7 ">
-          Unlock your potential with knowledge. Start learning today and pave your path to success with us.
+          Unlock your potential with knowledge. Start learning today and pave your path to success with uss.
           </p>
           {/* <p className="text-sm md:text-lg lg:text-sm font-poppins">Courses as low as $10</p> */}
         </div>
