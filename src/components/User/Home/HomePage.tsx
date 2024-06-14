8be866948a18429248b24a37993d044384fa7e50
asif-mohmd <asifasifpsps@gmail.com>
2024-06-14 19:55:44 +0530

    Env changed 4

--- src/components/User/Home/HomePage.tsx
+++ src/components/User/Home/HomePage.tsx
@@ -11,9 +11,9 @@ const HomePage: React.FC = () => {
           className="md:w-1/2 w-full h-auto md:order-last" // Move image to last on medium screens and up
         />
         <div className="md:w-1/2 p-6 md:p-12 md:order-first"> {/* Order first on medium screens and up */}
-          <h1 className="my-3 text-2xl md:text-3xl lg:text-5xl font-bold font-poppins">Learning comes to youuu</h1>
+          <h1 className="my-3 text-2xl md:text-3xl lg:text-5xl font-bold font-poppins">Learning comes to you</h1>
           <p className="text-sm md:text-lg lg:text-md font-poppins md:mt-7 ">
-          Unlock your potential with knowledge. Start learning today and pave your path to success with uss.
+          Unlock your potential with knowledge. Start learning today and pave your path to success with us.
           </p>
           {/* <p className="text-sm md:text-lg lg:text-sm font-poppins">Courses as low as $10</p> */}
         </div>
